9404a6bc2788f422dd613d3c962468da18e65c8b
Mac <macusa9@gmail.com>
2021-03-01 16:17:02 -0700

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+    required_providers {
+      azurerm = {
+          source  = "hashicorp/azurerm"
+          version = ">=2.26"
+      }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name     = "rg-iacdemo"
+    location = "westus2"
+}
+
+resource "azurerm_container_group" "cg" {
+    name                = "weatherapi"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_address_type = "public"
+    dns_name_label  = "macksterwa"
+    os_type         = "Linux"
+
+    container {
+        name   = "weatherapi"
+        image  = "mackster/weatherapi"
+        cpu    = "1"
+        memory = "1"
+
+        ports {
+            port     = 80
+            protocol = "TCP"
+        }
+    }
+}
